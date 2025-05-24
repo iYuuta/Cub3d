d0801a1b2f913627eae765f7b2708f63a6e2a7e5
Mohamed Amine Boulanouar <moboulan@e2r4p8.1337.ma>
2025-05-24 17:01:39 +0100

    element after map

--- parsing/init_map.c
+++ parsing/init_map.c
@@ -46,6 +46,8 @@ void	init_map(t_cube *cube)
 			&& ft_isallspace(current->next->content))
 			ft_error("Incorrect Map Structure: one or more empty lines");
 		check_characters(line);
+		if(!line)
+			ft_error("Element after Map");
 		cube->map.map[i++] = ft_strdup(line);
 		current = current->next;
 	}
--- utils/ft_isallspace.c
+++ utils/ft_isallspace.c
@@ -3,7 +3,9 @@
 int	ft_isallspace(char *str)
 {
 	size_t	i;
-
+	
+	if (!str)
+		return (0);
 	i = 0;
 	while (ft_isspace(str[i]))
 		i++;
